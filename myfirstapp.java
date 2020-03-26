69cd0245ac843afe5081ed7964139dd0a7ec2ae4
pzh-billsoft <62688190+pzh-billsoft@users.noreply.github.com>
2020-03-26 14:23:56 +0800

    Add files via upload

---
+++ myfirstapp.java
@@ -0,0 +1,21 @@
+/**
+ * @author  billsoft
+ * @date  ${DATE} ${TIME}
+ * @version 2020
+ */
+
+package bsoft;
+
+public class myfirstapp {
+    public static void main (String[] args){
+        int x = 1;
+        while (x<3){
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 3){
+            System.out.print("Do");
+        }
+    }
+}
